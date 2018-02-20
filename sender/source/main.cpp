01ecb547c255964fe81bf6c47794eac3f5ff90ea
greg <gregcj@hotmail.co.uk>
2018-02-20 23:35:44 +0000

    Changes made to sender.
    Changes to logic to sender, to send a dot the button needs to be held for 500ms and to send a dash greater than 1500ms, this changes how long the the pin is on.

--- sender/source/main.cpp
+++ sender/source/main.cpp
@@ -18,45 +18,51 @@
 MicroBitButton buttonA(MICROBIT_PIN_BUTTON_A, MICROBIT_ID_BUTTON_A);
 
 MicroBitPin P1(MICROBIT_ID_IO_P1, MICROBIT_PIN_P1, PIN_CAPABILITY_DIGITAL);
+
   int main()
   {
     // Initialise the micro:bit runtime.
     uBit.init();
 
+
     bool pressed = false;
 
-      while(1) {
+      while(1)
+      {
           // read current number of milliseconds
           uint64_t reading = system_timer_current_time();
 
           // loop while button A pressed
-          while (buttonA.isPressed()) {
+          while (buttonA.isPressed())
+          {
               pressed = true;
           }
 
           // time of loop execution
           uint64_t delta = system_timer_current_time() - reading;
 
-          // if button was pressed
-          if (pressed) {
-              // geater than a second
-              if (delta > 1000) { // > 1 sec
+          //if button was pressed
+          if (pressed)
+          {
+              //geater than a second = dash (a three ones)
+              if (delta > 1000)
+              { // > 1 sec
                   uBit.display.print("1");
                   P1.setDigitalValue(1);
-                  uBit.sleep(500);
+                  uBit.sleep(1500);
               }
-              // greater than 1/2 sec, but less than sec
-              else if (delta > 500) {
-                  uBit.display.print("0");
-                  P1.setDigitalValue(0);
-                  uBit.sleep(500);
+              //A dot, a single 1 for a single time unit(500ms)
+              else
+              {
+                P1.setDigitalValue(1);
+                ubit.sleep(500);
               }
+
 
               pressed = false;
               uBit.display.clear();
           }
       }
-
 
 
     //END OF PRE-GAME INITALISATION
