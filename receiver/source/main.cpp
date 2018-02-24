efde636d1ef48f5621c1a10a81b730f247984019
greg <gregcj@hotmail.co.uk>
2018-02-24 23:04:01 +0000

    Repair receiver.
    Binary string is now constructed. Need to work on decoding.

--- receiver/source/main.cpp
+++ receiver/source/main.cpp
@@ -10,24 +10,6 @@
 
   MicroBit uBit;
   MicroBitSerial serial(USBTX, USBRX);
-  //TODO 1. IMPLEMENT DECODING methods below.
-  //     2. Output decoded string on microbit.
-  /*
-//Decode the morse code string into ascii characters.
-std::string protocol::deCodeMorse(std::vector<std::string> morse)
-{
-	std::string decoded;
-
-	//Iterate through vector each string is one morse character.
-	for (size_t i = 0; i < morse.size(); i++)
-	{
-		decoded.push_back(binToChar[morse[i]]);
-	}
-
-	return decoded;
-}
-*/
-
 
 
 MicroBitPin P2(MICROBIT_ID_IO_P2, MICROBIT_PIN_P2, PIN_CAPABILITY_DIGITAL);
@@ -40,56 +22,76 @@ MicroBitPin P2(MICROBIT_ID_IO_P2, MICROBIT_PIN_P2, PIN_CAPABILITY_DIGITAL);
     //Every "tick" of this receiver the pins are read and either of these counters are incremented.
     int digLo =0;
     int digHi =0;
+    bool msg = false;
+
     //Decoded morse message, needs to be decoded again into ascii.
     std::vector<char> deMsg;
-    deMsg.push_back('|');
-    //uint64_t reading = system_timer_current_time();
 
-    //Message Loop.
+    //Binary String from pin.
+    std::vector<int> binStr;
     while(1)
     {
+
+
+    while(msg == true)
+    {
+      serial.baud(115200);
       if(P2.getDigitalValue() == 1)
       {
         //If 3 dots in a row with no space then its a dash.
-        if(digHi >= 3)
+        if(digHi > 2)
         {
-          //uBit.display.print("-");
-          deMsg.push_back('-');
-          //uBit.sleep(500);
+          uBit.display.print("-");
+          binStr.push_back(1);
+          serial.send('1');
+          uBit.sleep(500);
           digHi = 0;
           digLo = 0;
         }
         else
         {
           //Add a dot to the string
-          //uBit.display.print(".");
-          deMsg.push_back('.');
-          //uBit.sleep(500);
+          uBit.display.print(".");
+          binStr.push_back(1);
+          serial.send('1');
+          uBit.sleep(500);
           digHi++;
           digLo = 0;
         }
-
       }
 
-      if(digLo >= 7)
+      if(P2.getDigitalValue() == 0)
       {
-        uBit.display.print("End of message");
-        deMsg.push_back('|');
-      }
-      else
-      {
-        //Add a space
-        //uBit.display.print("s");
-        deMsg.push_back('0');
-        //uBit.sleep(500);
+        if(digLo >= 7)
+        {
+          uBit.display.print("C");
+          uBit.sleep(500);
+          msg = false;
+          break;
+        }
+        uBit.display.print("0");
+        binStr.push_back(1);
+        serial.send('0');
+        uBit.sleep(500);
         digLo++;
       }
 
       //Time Unit is 500 milliseconds.
       uBit.display.clear();
-      uBit.sleep(500);
     }
 
+    //Waiting for a signal.
+    while(msg == false)
+    {
+      uBit.display.print("W");
+      if(P2.getDigitalValue() == 1)
+      {
+        msg = true;
+        break;
+      }
+    }
+
+  }
   }
   //END OF FUNCTIONS
   //END OF CODE
